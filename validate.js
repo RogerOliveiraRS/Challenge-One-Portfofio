df33339adf2708fc9c8a16d33b08dfe3523a9590
RogerOliveiraRS <studio.rogeroliveira@gmail.com>
2024-05-02 10:55:31 -0300

    inclusão da validação form

--- validate.js
+++ validate.js
@@ -1,1 +1,16 @@
-//Seu JavaScript de validação aqui
+
+
+
+function validarFormulario() {
+    const nome = document.getElementById('nome').value;
+    const assunto = document.getElementById('assunto').value;
+
+    if (nome.trim() === '' || assunto.trim() === '') {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+        return false; // Impede o envio do formulário
+    }
+
+
+
+    return true; // Permite o envio do formulário
+}
